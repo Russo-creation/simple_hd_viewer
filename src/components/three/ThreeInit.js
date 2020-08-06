0f9929c9680ac361afe0b4dbcddbffa6b343b9b7
Lukasz Macon <luk.macon@gmail.com>
2020-08-06 19:17:10 +0200

    add three to webcack and create three init file

---
+++ src/components/three/ThreeInit.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ThreeInit = () => {
+  return (
+    <>
+      <div>hello world</div>
+    </>
+  );
+};
+
+export default ThreeInit;
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import ThreeInit from "../components/three/ThreeInit";
 
 function App() {
   return (
     <div className="App">
-      <div>hello word</div>
+      <ThreeInit />
     </div>
   );
 }
