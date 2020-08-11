0959f41cc67915e86d27d5ca77dbae64434d5def
Lukasz Macon <luk.macon@gmail.com>
2020-08-12 00:45:47 +0200

    Updates

---
+++ precache-manifest.bfcbd48e54657aabc16e78f6c1748353.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bfc856791793406deab1a22570cbf63",
+    "url": "/simple_hd_viewer/index.html"
+  },
+  {
+    "revision": "2a29c05a010935611127",
+    "url": "/simple_hd_viewer/static/css/main.d4618e7f.chunk.css"
+  },
+  {
+    "revision": "9a7bd80cbc3900e08cf8",
+    "url": "/simple_hd_viewer/static/js/2.6f908bcc.chunk.js"
+  },
+  {
+    "revision": "7ebaf537230ded90e67aac8a7e3b2641",
+    "url": "/simple_hd_viewer/static/js/2.6f908bcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a29c05a010935611127",
+    "url": "/simple_hd_viewer/static/js/main.984d14c2.chunk.js"
+  },
+  {
+    "revision": "58c2bdb8340c9f28eb21",
+    "url": "/simple_hd_viewer/static/js/runtime-main.00cba046.js"
+  },
+  {
+    "revision": "068c025942bf5f4ecd1fcba306fc098e",
+    "url": "/simple_hd_viewer/static/media/cave_wall_1k.068c0259.hdr"
+  },
+  {
+    "revision": "a8f21fd9d352b7ff001d84ae90432e01",
+    "url": "/simple_hd_viewer/static/media/room.a8f21fd9.zip"
+  },
+  {
+    "revision": "0f067bffd8e75a72d4529f7a689128f6",
+    "url": "/simple_hd_viewer/static/media/scratches.0f067bff.jpg"
+  }
+]);
