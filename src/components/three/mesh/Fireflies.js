e41b83d13da4e862eed45edf4c02bd5049456b77
Lukasz Macon <luk.macon@gmail.com>
2020-08-11 08:05:08 +0200

    add more fireflies and configure

--- src/components/three/mesh/Fireflies.js
+++ src/components/three/mesh/Fireflies.js
@@ -4,31 +4,86 @@ import * as THREE from "three";
 
 import Curve from "./Curve";
 
+const curveMoving = (
+  curveGeometry,
+  speed,
+  direction,
+  tanslationX,
+  tanslationY,
+  tanslationZ
+) => {
+  var time = Date.now();
+  var looptime = speed * 1000;
+  var t = (time % looptime) / looptime;
+
+  if (!direction) {
+    t = 1 - t;
+  }
+
+  var pos = curveGeometry.parameters.path.getPointAt(t);
+
+  pos.x += tanslationX; //left right
+  pos.y += tanslationY; //top bottom
+  pos.z += tanslationZ; //forweward backword
+  pos.multiplyScalar(0.001);
+
+  return pos;
+};
+
 const Fireflies = () => {
   //position={[-0.9027987733512952, 0.2278872503416854, -0.675830604006972]}
 
   const curveGeometry = new THREE.TubeBufferGeometry(Curve(), 100, 1, 1, true);
 
-  const group = useRef();
+  const group1 = useRef();
+  const group2 = useRef();
+  const group3 = useRef();
 
   useFrame((state, delta) => {
-    var time = Date.now();
-    var looptime = 20 * 1000;
-    var t = (time % looptime) / looptime;
-
-    var pos = curveGeometry.parameters.path.getPointAt(t);
-
-    pos.x += -902.7987733512952; //left right
-    pos.y += 150.8872503416854; //top bottom
-    pos.z += -705.830604006972; //forweward backword
-    pos.multiplyScalar(0.001);
-
-    group.current.position.copy(pos);
+    // 1st fireflie
+    group1.current.position.copy(
+      curveMoving(
+        curveGeometry,
+        20,
+        true,
+        -902.7987733512952,
+        150.8872503416854,
+        -705.830604006972
+      )
+    );
+    // 2st fireflie
+    group2.current.position.copy(
+      curveMoving(
+        curveGeometry,
+        10,
+        true,
+        -902.7987733512952,
+        150.8872503416854,
+        -705.830604006972
+      )
+    );
+    // 3st fireflie
+    group3.current.position.copy(
+      curveMoving(
+        curveGeometry,
+        15,
+        false,
+        -902.7987733512952,
+        150.8872503416854,
+        -705.830604006972
+      )
+    );
   });
 
   return (
     <>
-      <group ref={group}>
+      <group ref={group1}>
+        <FirefliesSet />
+      </group>
+      <group ref={group2}>
+        <FirefliesSet />
+      </group>
+      <group ref={group3}>
         <FirefliesSet />
       </group>
     </>
@@ -39,14 +94,16 @@ const FirefliesSet = () => {
   return (
     <>
       <mesh>
-        <sphereBufferGeometry attach="geometry" args={[0.001, 10, 10]} />
+        <sphereBufferGeometry attach="geometry" args={[0.001, 6, 6]} />
         <meshLambertMaterial
           attach="material"
           emissive="#ff840b"
           color="white"
+          opacity={0.6}
+          transparent={true}
         />
       </mesh>
-      <pointLight intensity={0.9} distance={0.3} decay={2} />
+      <pointLight intensity={0.9} distance={0.25} decay={2} />
     </>
   );
 };
