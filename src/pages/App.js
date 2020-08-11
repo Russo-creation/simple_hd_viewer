a6f4e5178c626c93867f289b0c750208f368b524
Lukasz Macon <luk.macon@gmail.com>
2020-08-11 22:58:48 +0200

    add fadeout to load window with react spring

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
 import ThreeInit from "../components/three/ThreeInit";
 import Loader from "../components/hud/Loader";
 
+import { Spring } from "react-spring/renderprops";
+
 import { connect } from "react-redux";
 
-const App = (props) => {
-  const { loadingFnished } = props;
+class App extends Component {
+  state = { displayLoadingWindow: true };
 
-  return (
-    <div className="App">
-      <ThreeInit />
-      {!loadingFnished ? <Loader /> : null}
-    </div>
-  );
-};
+  componentDidUpdate() {
+    if (this.props.loadingFnished) {
+      setTimeout(() => {
+        this.setState({
+          displayLoadingWindow: false,
+        });
+      }, 1000);
+    }
+  }
+
+  render() {
+    const { loadingFnished } = this.props;
+
+    return (
+      <div className="App">
+        <ThreeInit />
+
+        {this.state.displayLoadingWindow ? (
+          !loadingFnished ? (
+            <Loader />
+          ) : (
+            <Spring from={{ opacity: 1 }} to={{ opacity: 0 }} duration={1000}>
+              {(props) => (
+                <div style={props}>
+                  <Loader />
+                </div>
+              )}
+            </Spring>
+          )
+        ) : null}
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (state) => {
   return {
     loadingFnished: state.loader.loadingFnished,
+    displayWindow: state.loader.displayWindow,
   };
 };
 
 export default connect(mapStateToProps)(React.memo(App));
-
-//export default App;
